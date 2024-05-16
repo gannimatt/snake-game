681db5285079e98c3c2905d73255b542108e0c48
ganimatt <ganimatisayevv@gmail.com>
2024-05-16 21:16:57 +0200

    deleted tk

--- tests/test_game.py
+++ tests/test_game.py
@@ -2,7 +2,7 @@ import sys
 import os
 import unittest
 from unittest.mock import MagicMock
-from tkinter import Tk
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
